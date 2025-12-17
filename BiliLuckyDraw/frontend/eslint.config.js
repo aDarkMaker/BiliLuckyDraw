1481a29e387a718b4203b6d4e151b0ba6a7d9cfd
aDarkMaker <2261265112@qq.com>
2025-12-17 21:57:27 +0800

    frontend init

---
+++ BiliLuckyDraw/frontend/eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+import tseslint from 'typescript-eslint';
+import vueParser from 'vue-eslint-parser';
+
+export default [
+    { ignores: ['dist/**', 'node_modules/**', 'wailsjs/**', '*.d.ts'] },
+
+    js.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    ...vue.configs['flat/recommended'],
+
+    {
+        files: ['**/*.{ts,tsx,vue,js}', 'src/vite-env.d.ts'],
+        languageOptions: {
+            parser: vueParser,
+            parserOptions: {
+                parser: tseslint.parser,
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            'vue/multi-word-component-names': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-empty-object-type': 'off',
+        },
+    },
+];
